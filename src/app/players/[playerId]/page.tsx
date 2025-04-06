299398f1676d1c86b76a2941b28239da30025696
Christoph Wiese <32820890+WieseChristoph@users.noreply.github.com>
2025-04-06 17:59:59 +0200

    Fix header again

--- src/app/players/[playerId]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default async function PlayerInfo({ params }: { params: Promise<{ playerId: number }> }) {
-	const { playerId } = await params;
-
-	return <div>Player info of {playerId}</div>;
-}
--- src/app/players/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PlayerInfo() {
-	return <div>Player infos</div>;
-}
